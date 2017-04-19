f933008212bc4b6a7531b66970be0f88cf7e23d8
Darius Kazemi <darius.kazemi@gmail.com>
2017-04-19 15:15:09 -0700

    debugging

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -14,8 +14,12 @@ class PostStatusService < BaseService
   # @return [Status]
   def call(account, text, in_reply_to = nil, options = {})
     media  = validate_media!(options[:media_ids])
+    puts "HI"
+    puts text
     text_without_urls= text.gsub(/http.?:\/\/[^\s\\]+/, '')
+    puts text_without_urls
     text_without_urls= text_without_urls.gsub(/@[^\s\\]+@[^\s\\]+\.[a-z]+/, '')
+    puts text_without_urls
     raise Mastodon::ValidationError, 'Invalid symbol' if text_without_urls.gsub(/[^[:word:]]|[0-9]/,'').gsub('_','').match(/[^eE]/)
     raise Mastodon::ValidationError, 'Invalid symbol' if options[:spoiler_text].gsub(/[^[:word:]]|[0-9]/,'').gsub('_','').match(/[^eE]/)
     status = account.statuses.create!(text: text,
